57da26871733fe23ff6f30d45c996207d7b0ceda
yoshizawa <dekosan117117@gmail.com>
2026-06-20 10:48:25 +0900

    SECRET_KEYを生成して設定

--- auth.py
+++ auth.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+import os
 
-SECRET_KEY = "your-secret-key-here"
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-dev-key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
